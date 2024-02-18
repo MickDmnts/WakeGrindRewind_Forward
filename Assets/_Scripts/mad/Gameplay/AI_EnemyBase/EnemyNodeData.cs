5987e3a1844fdc024b63e23d3ede8e664bc13d82
Michael Evangelos Diamantis <michaeldiamantis17@gmail.com>
2024-02-18 13:51:03 +0200

    Various fixes

--- Assets/_Scripts/mad/Gameplay/AI_EnemyBase/EnemyNodeData.cs
+++ Assets/_Scripts/mad/Gameplay/AI_EnemyBase/EnemyNodeData.cs
@@ -1,25 +1,44 @@
 using UnityEngine;
 
 namespace WGRF.AI
-{
+{   
+    /// <summary>
+    /// Caches the agents node data information
+    /// </summary>
     public class EnemyNodeData : INodeData
-    {
+    {   
+        ///<summary>Reference to the agent handler</summary>
         AIEntity enemyEntity;
 
+        ///<summary>The layers the agent traverses around</summary>
         LayerMask occlusionLayers;
+        ///<summary>Can the agent attack?</summary>
         bool canAttack;
+        ///<summary>Can the agent shoot?</summary>
         bool canShoot;
 
+        /// <summary>
+        /// Creates an enemy node data instance
+        /// </summary>
+        /// <param name="enemyEntity">The agent handler of this cache</param>
         public EnemyNodeData(EnemyEntity enemyEntity)
         { this.enemyEntity = enemyEntity; }
 
+        ///<summary>Returns the agent of this data cache</summary>
         public AIEntity EnemyEntity => enemyEntity;
+        ///<summary>Returns the enemy animation handler</summary>
         public AIEntityAnimations EnemyAnimations => enemyEntity.Controller.Access<EnemyAnimations>("eAnimations");
+        ///<summary>Returns the enemy occlusion layers</summary>
         public LayerMask OcclusionLayers => occlusionLayers;
+        ///<summary>Is this agent dead?</summary>
         public bool IsDead { get => enemyEntity.IsDead; set { enemyEntity.IsDead = value; } }
+        ///<summary>Can this agent attack?</summary>
         public bool CanAttack { get => canAttack; set { canAttack = value; } }
+        ///<summary>Can this agent shoot?</summary>
         public bool CanShoot { get => canShoot; set { canShoot = value; } }
+        ///<summary>This agent's target</summary>
         public Transform Target { get => ((EnemyEntity)enemyEntity).Target; }
+        ///<summary>The equiped weapon range</summary>
         public float WeaponRange { get => enemyEntity.Controller.Access<EnemyWeapon>("eWeapon").equipedWeapon.MinShootDistance; }
     }
 }
--- Assets/_Scripts/mad/Gameplay/AI_EnemyBase/EnforcerNodeData.cs
+++ Assets/_Scripts/mad/Gameplay/AI_EnemyBase/EnforcerNodeData.cs
@@ -2,25 +2,42 @@ using UnityEngine;
 
 namespace WGRF.AI
 {
+    /// <summary>
+    /// Caches the agents node data information
+    /// </summary>
     public class EnforcerNodeData : INodeData
     {
+        ///<summary>Reference to the agent handler</summary>
         AIEntity enforcerEntity;
-
+        ///<summary>The layers the agent traverses around</summary>
         LayerMask occlusionLayers;
+        ///<summary>Can the agent protect?</summary>
         bool canProtect;
-
+        ///<summary>The current lowest health enemy</summary>
         AIEntity lowestHpEnemy;
 
+        /// <summary>
+        /// Creates an enemy node data instance
+        /// </summary>
+        /// <param name="enemyEntity">The agent handler of this cache</param>
         public EnforcerNodeData(EnforcerEntity enforcerEntity)
         { this.enforcerEntity = enforcerEntity; }
 
+        ///<summary>Returns the agent of this data cache</summary>
         public AIEntity EnemyEntity => enforcerEntity;
+        ///<summary>Returns the enemy animation handler</summary>
         public AIEntityAnimations EnemyAnimations => enforcerEntity.Controller.Access<EnforcerAnimations>("eAnimations");
+        ///<summary>Returns the enemy occlusion layers</summary>
         public LayerMask OcclusionLayers => occlusionLayers;
+        ///<summary>Is this agent dead?</summary>
         public bool IsDead { get => enforcerEntity.IsDead; set { enforcerEntity.IsDead = value; } }
+        ///<summary>Can the agent protect?</summary>
         public bool CanProtect { get => canProtect; set { canProtect = value; } }
+        ///<summary>This agent's target</summary>
         public Transform Target { get => ((EnforcerEntity)enforcerEntity).Target; }
+        ///<summary>The equiped weapon range</summary>
         public float WeaponRange { get => enforcerEntity.Controller.Access<EnemyWeapon>("eWeapon").equipedWeapon.MinShootDistance; }
+        ///<summary>The current lowest health enemy</summary>
         public AIEntity LowestHpEnemy { get => lowestHpEnemy; set => lowestHpEnemy = value; }
     }
 }
